d4a332da9150e69295efccb517d903ca9d7b22ea
Tom Castagnino <tcastagnino@gmail.com>
2020-08-15 15:26:28 -0700

    Adding tests for interactions and rendering of Form component

---
+++ src/components/Form.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Form from './Form';
+import { shallow } from 'enzyme';
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = shallow(<Form />);
+});
+
+describe('<Form> rendering', () => {
+    it('should render  one <form>', () => {
+        expect(wrapper.find('form')).toHaveLength(1);
+    });
+    it('should not render any button when operator is not pass in props', () => {
+        expect(wrapper.find('button')).toHaveLength(0);
+    });
+    it('should render one Add button when the + operator is passed as prop', () => {
+        wrapper.setProps( { operator: '+'} );
+        expect(wrapper.find('#formButtonAdd')).toHaveLength(1);
+        expect(wrapper.find('#formButtonSubstract')).toHaveLength(0);
+    });
+    it('should render one Substract button when the - operator is passed as prop', () => {
+        wrapper.setProps( { operator: '-'} );
+        expect(wrapper.find('#formButtonAdd')).toHaveLength(0);
+        expect(wrapper.find('#formButtonSubstract')).toHaveLength(1);
+    });
+});
+
+describe('<Form interactions', () => {
+    it('should change the firstNumber state when onChange function of the #number1 input is invoked', () => {
+        wrapper.find('#number1').simulate('change', { target: {value: 50} });
+        expect(wrapper.state('firstNumber')).toEqual(50);
+        expect(wrapper.state('secondNumber')).toEqual('');
+    });
+    it('should change the secondNumber state when onChange function of the #number2 input is invoked', () => {
+        wrapper.find('#number2').simulate('change', { target: {value: 70} });
+        expect(wrapper.state('firstNumber')).toEqual('');
+        expect(wrapper.state('secondNumber')).toEqual(70);
+    });
+});
